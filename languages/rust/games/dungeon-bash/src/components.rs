0f934bf84f895ccae9c9d83dd7ca29ebbb88de8c
Tim Langford <tim.langford@gmail.com>
2021-12-06 10:35:05 +0000

    Updated to use Legion ECS.

---
+++ languages/rust/games/dungeon-bash/src/components.rs
@@ -0,0 +1,17 @@
+pub use crate::prelude::*;
+
+// Components -----------------------------------------------------------------
+//
+
+// Render
+//
+#[derive(Clone, Copy, Debug, PartialEq)]
+pub struct Render {
+    pub color: ColorPair,
+    pub glyph: FontCharType,
+}
+
+// Player
+//
+#[derive(Clone, Copy, Debug, PartialEq)]
+pub struct Player;
--- languages/rust/games/dungeon-bash/src/main.rs
+++ languages/rust/games/dungeon-bash/src/main.rs
@@ -1,35 +1,50 @@
 mod camera;
+mod components;
 mod map;
 mod map_builder;
-mod player;
+mod spawner;
+mod systems;
 mod prelude {
     pub use bracket_lib::prelude::*;
+    pub use legion::systems::CommandBuffer;
+    pub use legion::world::SubWorld;
+    pub use legion::*;
+
+    pub use crate::camera::*;
+    pub use crate::components::*;
+    pub use crate::map::*;
+    pub use crate::map_builder::*;
+    pub use crate::spawner::*;
+    pub use crate::systems::*;
+
     pub const SCREEN_WIDTH: i32 = 80;
     pub const SCREEN_HEIGHT: i32 = 50;
     pub const DISPLAY_WIDTH: i32 = SCREEN_WIDTH / 2;
     pub const DISPLAY_HEIGHT: i32 = SCREEN_HEIGHT / 2;
-    pub use crate::camera::*;
-    pub use crate::map::*;
-    pub use crate::map_builder::*;
-    pub use crate::player::*;
 }
 use prelude::*;
 
 // State ------------------------------------------------------------------------
 //
 struct State {
-    map: Map,
-    player: Player,
-    camera: Camera,
+    ecs: World,
+    resources: Resources,
+    systems: Schedule,
 }
+
 impl State {
     fn new() -> Self {
+        let mut ecs = World::default();
+        let mut resources = Resources::default();
         let mut rng = RandomNumberGenerator::new();
         let map_builder = MapBuilder::new(&mut rng);
+        spawn_player(&mut ecs, map_builder.player_start);
+        resources.insert(map_builder.map);
+        resources.insert(Camera::new(map_builder.player_start));
         Self {
-            map: map_builder.map,
-            player: Player::new(map_builder.player_start),
-            camera: Camera::new(map_builder.player_start),
+            ecs,
+            resources,
+            systems: build_scheduler(), // TODO
         }
     }
 }
@@ -43,9 +58,12 @@ impl GameState for State {
         ctx.cls();
         ctx.set_active_console(1);
         ctx.cls();
-        self.player.update(ctx, &self.map, &mut self.camera);
-        self.map.render(ctx, &self.camera);
-        self.player.render(ctx, &self.camera);
+        // Add keyboard state as a resource.
+        self.resources.insert(ctx.key);
+        // Execute Systems
+        self.systems.execute(&mut self.ecs, &mut self.resources);
+        // Render Draw Buffer
+        render_draw_buffer(ctx).expect("Render error");
     }
 }
 
