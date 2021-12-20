832d4b9da96720082b75c66cf7a5c0372bb4d702
Tim Langford <tim.langford@gmail.com>
2021-12-20 16:40:37 +0000

    Handle 'Game Over' scenario.

--- languages/rust/games/dungeon-bash/src/main.rs
+++ languages/rust/games/dungeon-bash/src/main.rs
@@ -61,6 +61,49 @@ impl State {
             monster_systems: build_monster_scheduler(),
         }
     }
+
+    fn game_over(&mut self, ctx: &mut BTerm) {
+        ctx.set_active_console(2);
+        ctx.print_color_centered(2, RED, BLACK, "Your quest has ended.");
+        ctx.print_color_centered(
+            4,
+            WHITE,
+            BLACK,
+            "Slain by a yucky monster, your hero's journey has come to a \
+            premature end.",
+        );
+        ctx.print_color_centered(
+            5,
+            WHITE,
+            BLACK,
+            "The Amulet of Wotnot remains unclaimed, and your home town \
+            of Wibbleton is not saved.",
+        );
+        ctx.print_color_centered(
+            8,
+            YELLOW,
+            BLACK,
+            "Don't worry, you can always try again with a new hero.",
+        );
+        ctx.print_color_centered(9, GREEN, BLACK, "Press 1 to play again.");
+
+        if let Some(VirtualKeyCode::Key1) = ctx.key {
+            self.ecs = World::default();
+            self.resources = Resources::default();
+            let mut rng = RandomNumberGenerator::new();
+            let map_builder = MapBuilder::new(&mut rng);
+            spawn_player(&mut self.ecs, map_builder.player_start);
+            map_builder
+                .rooms
+                .iter()
+                .skip(1)
+                .map(|r| r.center())
+                .for_each(|pos| spawn_monster(&mut self.ecs, &mut rng, pos));
+            self.resources.insert(map_builder.map);
+            self.resources.insert(Camera::new(map_builder.player_start));
+            self.resources.insert(TurnState::AwaitingInput);
+        }
+    }
 }
 
 // Game State -----------------------------------------------------------------
@@ -97,6 +140,9 @@ impl GameState for State {
             TurnState::MonsterTurn => self
                 .monster_systems
                 .execute(&mut self.ecs, &mut self.resources),
+            TurnState::GameOver => {
+                self.game_over(ctx);
+            }
         }
         // Render Draw Buffer
         render_draw_buffer(ctx).expect("Render error");
