68c614599e264acc93702d69cd06fdfdfbe297e4
Tim Langford <tim.langford@gmail.com>
2022-04-10 16:37:14 +0100

    Added basic solver.

--- game-dev/java-script/p5-js/the-coding-train/dfs-maze-generator/sketch.js
+++ game-dev/java-script/p5-js/the-coding-train/dfs-maze-generator/sketch.js
@@ -3,6 +3,8 @@ class Cell {
     visited = false;
     start = false;
     finished = false;
+    ingress = false;
+    egress = false;
 
     // TRBL - [Top, Right, Bottom, Left]
     walls = [true, true, true, true];
@@ -64,6 +66,16 @@ class Cell {
         if (this.start) {
             color = props.cells.start;
         }
+        if (this.ingress) {
+            color = props.cells.ingress;
+        }
+        if (this.egress) {
+            color = props.cells.egress;
+        }
+        if (this.solution) {
+            color = 'yellow';
+        }
+
         return color;
     };
 }
@@ -93,6 +105,31 @@ class Grid {
             cell = this.cells[idx];
         }
         return cell;
+    };
+
+    findStart = () => {
+        return this.cells.filter((c) => c.ingress);
+    };
+
+    getAllNeighbours = (cell) => {
+        const neighbours = [];
+        const tc = this.getCell(cell.col, cell.row - 1);
+        const rc = this.getCell(cell.col + 1, cell.row);
+        const bc = this.getCell(cell.col, cell.row + 1);
+        const lc = this.getCell(cell.col - 1, cell.row);
+        if (tc) {
+            neighbours.push(tc);
+        }
+        if (rc) {
+            neighbours.push(rc);
+        }
+        if (bc) {
+            neighbours.push(bc);
+        }
+        if (lc) {
+            neighbours.push(lc);
+        }
+        return neighbours;
     };
 
     getAllUnvisitedNeighbours = (cell) => {
@@ -133,9 +170,50 @@ class Grid {
         return selected;
     };
 
+    move = (fromCell, toCell) => {
+        fromCell.current = false;
+        toCell.current = true;
+        toCell.visited = true;
+    };
+
     tunnel = (fromCell, toCell) => {
         this._removeWalls(fromCell, toCell);
-        this._move(fromCell, toCell);
+        this.move(fromCell, toCell);
+    };
+
+    addBegin = () => {
+        const begin = this.getCell(Math.floor(random(0, this.cols)), this.rows - 1);
+        begin.walls[2] = false;
+        begin.ingress = true;
+        return begin;
+    };
+
+    addFinish = () => {
+        const finish = this.getCell(Math.floor(random(0, this.cols)), 0);
+        finish.walls[0] = false;
+        finish.egress = true;
+        return finish;
+    };
+
+    hasWall = (fromCell, toCell) => {
+        const colDiff = fromCell.col - toCell.col;
+        const rowDiff = fromCell.row - toCell.row;
+        if (colDiff > 0) {
+            // RIGHT
+            return fromCell.walls[3] || toCell.walls[1];
+        }
+        if (colDiff < 0) {
+            // LEFT
+            return fromCell.walls[1] || toCell.walls[3];
+        }
+        if (rowDiff > 0) {
+            // TOP
+            return fromCell.walls[0] || toCell.walls[2];
+        }
+        if (rowDiff < 0) {
+            // BOTTOM
+            return fromCell.walls[2] || toCell.walls[0];
+        }
     };
 
     _removeWalls = (fromCell, toCell) => {
@@ -161,12 +239,6 @@ class Grid {
             fromCell.walls[2] = false;
             toCell.walls[0] = false;
         }
-    };
-
-    _move = (fromCell, toCell) => {
-        fromCell.current = false;
-        toCell.current = true;
-        toCell.visited = true;
     };
 
     draw = () => {
@@ -208,6 +280,75 @@ class RandomizedDFSGenerator {
             this.generateNext();
         }
     };
+
+    done = () => {
+        return this.stack.length === 0;
+    };
+}
+
+class RandomizedDFSSolver {
+    constructor(grid) {
+        // Create Solver
+        // const maze = new Grid(grid.cols, grid.rows, grid.cellSize);
+        // this.grid = maze;
+        // this.begin = maze.addBegin();
+        // this.finish = maze.addFinish();
+        this.grid = grid;
+        this.begin = this.grid.addBegin();
+        this.finish = this.grid.addFinish();
+
+        for (let j = 0; j < this.grid.cols; j++) {
+            for (let i = 0; i < this.grid.rows; i++) {
+                // this.cells.push(new Cell(i, j, cellSize));
+                const cell = this.grid.getCell(i, j);
+                cell.visited = false;
+                this.finished = false;
+            }
+        }
+
+        // Initialise
+        this.stack = [];
+        this.begin.start = true;
+        this.begin.visited = true;
+        this.stack.push(this.begin);
+        this.solved = false;
+    }
+
+    solveNext = () => {
+        if (this.stack.length > 0 && !this.solved) {
+            const currentCell = this.stack.pop();
+
+            const neighbours = this.grid.getAllNeighbours(currentCell);
+            const reachable = neighbours.filter((toNeighbour) => {
+                return !this.grid.hasWall(currentCell, toNeighbour);
+            });
+            const unvisited = reachable.filter((candidate) => {
+                if (!candidate.visited) {
+                    return candidate;
+                }
+            });
+            // Could randomly select the next cell?
+            const nextCell = unvisited[0];
+
+            if (nextCell) {
+                if (nextCell === this.finish) {
+                    this.solved = true;
+                    this.stack.push(currentCell);
+                    this.stack.forEach((cell) => (cell.solution = true));
+                    // console.log('Finished! Solved the maze');
+                } else {
+                    this.stack.push(currentCell);
+                    this.grid.move(currentCell, nextCell);
+                    this.stack.push(nextCell);
+                }
+            } else {
+                // currentCell.current = false;
+                // currentCell.finished = true;
+            }
+        }
+    };
+
+    solveAll = () => {};
 }
 
 // Canvas Configuration ---------------------------------------------------------------------------
@@ -222,14 +363,34 @@ const CANVAS_PRIMARY = 0;
 // p5 Instance ------------------------------------------------------------------------------------
 //
 
-let rows = 40;
-let cols = 40;
-let cellSize = Math.floor(CANVAS_WIDTH / cols);
-
 let animate = false;
 
+// let rows = 40;
+// let cols = 40;
+let rows = 10;
+let cols = 10;
+let cellSize = Math.floor(CANVAS_WIDTH / cols);
+
 let generator;
+let solver;
 const props = {};
+
+const createPalette = (hexCol) => {
+    const r = parseInt(hexCol.slice(1, 3), 16);
+    const g = parseInt(hexCol.slice(3, 5), 16);
+    const b = parseInt(hexCol.slice(5), 16);
+    const a = 1;
+    props.cells = {
+        unvisited: CANVAS_BACKGROUND,
+        current: color('rgba(255, 0, 0, 0.75)'),
+        start: color(`rgba(${r}, ${g}, ${b}, ${a})`),
+        visited: color(`rgba(${r}, ${g}, ${b}, ${a - 0.4})`),
+        finished: color(`rgba(${r}, ${g}, ${b}, ${a - 0.6})`),
+        end: color(`rgba(${r}, ${g}, ${b}, ${a - 0.8})`),
+        ingress: 'blue',
+        egress: 'red',
+    };
+};
 
 const clearMaze = () => {
     generator = new RandomizedDFSGenerator(cols, rows, cellSize);
@@ -255,43 +416,37 @@ function setup() {
 
     // Maze grid size controller.
     const gridSizeSelect = document.getElementById('grid-size');
-    gridSizeSelect.onchange = () => {
-        const parts = gridSizeSelect.value.split('x');
+    gridSizeSelect.onchange = (e) => {
+        const parts = e.target.value.split('x');
         cols = parts[0];
         rows = parts[1];
         cellSize = Math.floor(CANVAS_WIDTH / cols);
         generator = new RandomizedDFSGenerator(cols, rows, cellSize);
+        solver = null;
     };
+
+    // Maze colour controller.
+    const mazeColorSelect = document.getElementById('maze-color');
+    mazeColorSelect.onchange = (e) => {
+        const newHexCol = e.target.value;
+        createPalette(newHexCol);
+    };
+    createPalette(mazeColorSelect.value);
 
     // Maze generation animation controller.
     const animateCheckbox = document.getElementById('animate-maze-gen');
-    animateCheckbox.onchange = () => {
-        animate = animateCheckbox.checked;
+    animateCheckbox.onchange = (e) => {
+        animate = e.target.checked;
     };
 
     // Maze generator button.
     const generateMazeBtn = document.getElementById('generate-maze-btn');
     generateMazeBtn.onclick = () => {
         generateMaze();
+        solver = null;
     };
 
-    // Pink Pallette
-    // const r = 255;
-    // const g = 128;
-    // const b = 255;
-    const r = 0;
-    const g = 256;
-    const b = 0;
-    const a = 1;
-    props.cells = {
-        unvisited: CANVAS_BACKGROUND,
-        current: color('rgba(255, 0, 0, 0.75)'),
-        start: color(`rgba(${r}, ${g}, ${b}, ${a})`),
-        visited: color(`rgba(${r}, ${g}, ${b}, ${a - 0.4})`),
-        finished: color(`rgba(${r}, ${g}, ${b}, ${a - 0.6})`),
-        end: color(`rgba(${r}, ${g}, ${b}, ${a - 0.8})`),
-    };
-
+    // Maze Generator
     generator = new RandomizedDFSGenerator(cols, rows, cellSize);
     if (!animate) {
         generator.generateAll();
@@ -300,8 +455,22 @@ function setup() {
 
 function draw() {
     background(CANVAS_BACKGROUND);
+
+    // Generate Maze
+    //
     if (animate) {
         generator.generateNext();
     }
     generator.grid.draw();
+
+    // Solve Maze
+    //
+    if (generator.done()) {
+        if (!solver) {
+            solver = new RandomizedDFSSolver(generator.grid);
+        }
+        if (animate) {
+            solver.solveNext();
+        }
+    }
 }
